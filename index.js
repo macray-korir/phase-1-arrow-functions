f7ef952f965b0882ffe6bdb41fd107d57d63e2f7
macray <macraykorir254@gmail.com>
2023-06-16 08:58:09 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+// Define the divide function expression
+const divide = function () {
+  return 2000 / 100;
+};
+
+// Define the square arrow function
+const square = (num) => num * num;
+
+// Define the add arrow function
+const add = (num1, num2) => num1 + num2;
